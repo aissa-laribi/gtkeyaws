53da6357369e32ebfaa91acc3f860b4fba33a363
aissa-laribi <aissalaribi@yahoo.fr>
2022-03-02 00:43:50 +0000

    Browsed to Credentials Section

--- gtkeyaws.py
+++ gtkeyaws.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import requests
 import time
 browser = webdriver.Firefox(executable_path="./drivers/geckodriver")
 
-browser.get('https://654175463099.signin.aws.amazon.com/console?region=us-east-1')
+browser.get('https://ACCOUNTNUMBER.signin.aws.amazon.com/console?region=us-east-1')
 
-browser.find_element_by_xpath('//*[@id="username"]').send_keys('cloud_user')
+browser.find_element_by_xpath('//*[@id="username"]').send_keys('USERNAME')
 time.sleep(2)
 
-browser.find_element_by_xpath('//*[@id="password"]').send_keys('%0Ajeshgec')
+browser.find_element_by_xpath('//*[@id="password"]').send_keys('PASSWORD')
 time.sleep(2)
 
 browser.find_element_by_xpath('//*[@id="signin_button"]').click()
